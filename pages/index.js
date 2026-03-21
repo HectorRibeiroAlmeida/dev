0b856fad058a7835e443c46473bf7f3445950ed4
Hector Almeida <hector.almeida.dev@gmail.com>
2026-03-21 00:39:05 -0300

    Segundo Deploy testando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Karol, eu amo muito você, se você me ama, da uma risadinha</h1>
+    return <h1>Karol, se você me ama, da uma risadinha, e coloca mais um ep de anne</h1>
 }   
 
 export default Home;
