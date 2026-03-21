0c0d2fd85bdc3ad713d167d8957ba28c2286d36a
Hector Almeida <hector.almeida.dev@gmail.com>
2026-03-21 00:35:20 -0300

    Testando meu primeiro deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Se você está vendo isso aqui, provavelmente funcionou</h1>
+    return <h1>Karol, eu amo muito você, se você me ama, da uma risadinha</h1>
 }   
 
 export default Home;
