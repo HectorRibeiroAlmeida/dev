4b816afc4f3e00644dcdeb34a561007171934abe
Hector Almeida <hector.almeida.dev@gmail.com>
2026-03-23 04:28:50 -0300

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Karol, se você me ama, da uma risadinha, e coloca mais um ep de anne</h1>
-}   
+function Home() {
+  return (
+    <h1>
+      Karol, se você me ama, da uma risadinha, e coloca mais um ep de anne
+    </h1>
+  );
+}
 
 export default Home;
